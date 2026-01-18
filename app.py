65783eda050f05c9c221c4f79d543be8d6f8cdde
unknown <_@gmail.com>
2026-01-18 22:29:55 +0400

    Finally

--- app.py
+++ app.py
@@ -6,20 +6,10 @@ from flask import redirect, url_for
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/delete_movie/<int:movie_id>')
-def delete_movie(movie_id):
-    movie = Movie.query.get_or_404(movie_id)
-    db.session.delete(movie)
-    db.session.commit()
-    return redirect(url_for('index'))
-
-import routes
-
-# --- ცვლილება აქ არის ---
-# ეს ნაწილი ამოვიტანეთ main-დან გარეთ, რომ Render-მა დაინახოს
 with app.app_context():
     db.create_all()
-# ------------------------
+
+import routes
 
 if __name__ == "__main__":
     app.run(debug=True)
