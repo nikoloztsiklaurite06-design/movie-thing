27eb25497a4ad211f14a2291314a9767865446b9
unknown <_@gmail.com>
2026-01-18 22:14:15 +0400

    Fixed my life

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from ext import app, db, login_manager
-from models import User
+from models import User, Movie
+from flask import redirect, url_for
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-import routes
+@app.route('/delete_movie/<int:movie_id>')
+def delete_movie(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+with app.app_context():
+    db.create_all()
+# ------------------------
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
